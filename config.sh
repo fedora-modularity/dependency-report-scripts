0a4d9d9215e48fc04c99fb5e15f352fcd0d5e731
Nils Philippsen <nils@redhat.com>
2017-08-25 13:24:14 +0200

    make defaults better maintainable

--- config.sh
+++ config.sh
@@ -1,11 +1,27 @@
 # This file contains defaults. Put local configuration into the file
 # config_local.sh.
 
-_thisdir="$(dirname "$BASH_SOURCE")"
-_topdir_override="${topdir:-}"
+# Defaults go here. These will be eval'ed, therefore use single quotes.
 
-if [ -e "$_thisdir/config_local.sh" ]; then
-    source "$_thisdir/config_local.sh"
-fi
+CONFIG_DEFAULTS='
+topdir=${thisdir}/output
+'
 
-topdir="${_topdir_override:-${topdir:-$_thisdir/output}}"
+# No user-serviceable parts below.
+
+# Needs to be a function for 'local' to work.
+_configure_the_things() {
+    local thisdir="$(dirname "$BASH_SOURCE")"
+
+    local topdir_override="${topdir:-}"
+
+    if [ -e "$thisdir/config_local.sh" ]; then
+        source "$thisdir/config_local.sh"
+    fi
+
+    eval "$(echo "$CONFIG_DEFAULTS" | sed -e 's/^\([^=]\+\)=\(.*\)/local \1_default=\2/g')"
+
+    topdir="${topdir_override:-${topdir:-$topdir_default}}"
+}
+
+_configure_the_things
